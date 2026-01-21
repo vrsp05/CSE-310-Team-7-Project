26b512af5ea46fa619ba41b02d33fffb55187c0a
Daniel Acosta <danielacosta@byui.edu>
2026-01-21 13:08:09 -0700

    added routes in my app.js

--- app.js
+++ app.js
@@ -8,10 +8,22 @@ app.set("views", "./views");
 // public files
 app.use(express.static("public"));
 
+// ROUTES
+
 // main route
 app.get("/", (req, res) => {
   res.render("index");
 });
+//register page
+app.get("/register", (req,res) => {
+    res.render("auth/register");
+});
+// dashboard page
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard/index");
+});
+
+
 
 // server
 app.listen(3000, () => {
