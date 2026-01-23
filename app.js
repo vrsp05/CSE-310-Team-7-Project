b6ec646fcafd0edd236217a2c83d70f04663676d
Daniel Acosta <danielacosta@byui.edu>
2026-01-23 13:14:30 -0700

    edits to deploy on render

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.get("/dashboard", (req, res) => {
 
 
 // server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
