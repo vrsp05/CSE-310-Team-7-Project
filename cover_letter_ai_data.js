ddcf98afdb02de5e327c356785e714a87f4e05c3
Victor Rafael Santana Pimentel <victorrafaelsantana@hotmail.es>
2026-01-30 01:55:11 -0700

    Added more AI training data

---
+++ cover_letter_ai_data.js
@@ -0,0 +1,345 @@
+const COVER_LETTER_DATA = {
+
+cover_letter_content: 
+
+`COVER LETTER CHECKLIST:
+Some employers still see cover letters as a powerful way for applicants to 
+demonstrate their written communication skills and present themselves as an 
+ideal candidate. Since there are many formats and alterations used when 
+writing cover letters, the guidelines below are general enough to be 
+appropriate for most situations. However, always remember that a cover 
+letter should be tailored to the needs and expectations of each specific 
+audience.
+Click here for cover letter samples, as well as sample email and LinkedIn 
+messages to employers.
+FORMAT/MECHANICS	
+Format
+◻	Is the font appealing, easy to read, and appropriate for the audience?
+◻	Have items such as the following been included appropriately for the 
+  chosen format and audience?
+o	Name and contact information (of applicant and addressee)
+o	Date
+o	Opening and closing salutations
+o	Signature block (“Enclosure” phrase is only needed with hard copy 
+  submissions, which are rare)
+◻	Is there consistency in abbreviations, spacing, margins, etc.?
+Mechanics of Cover Letter
+◻	Are there any spelling, punctuation, or grammatical errors?
+◻	Has the applicant used professional wording that is appropriate to 
+  his/her audience? (Say ‘NO!’ to texting lingo.)
+◻	Has the applicant used word variety in the letter?
+o	Are there any words or phrases that are redundant or over-used? 
+  (Avoid over-usage of the word “I”)
+◻	Do the sentences flow?
+o	Are they easy to read, rather than confusing, ambiguous, and/or 
+  cumbersome?
+◻	Are transitions between paragraphs logical and effective? Or are they 
+  choppy/random?
+ 	CONTENT	
+The Introductory Paragraph
+◻	Explains the purpose of the letter (i.e., the position being sought)
+◻	Includes a powerful “grabber” that:
+1.	Shows the applicant’s vision, knowledge of the company, and/or how 
+  the applicant might fit in OR
+2.	Is a general statement about how the applicant will help the 
+  organization achieve its goals through his/her skills
+◻	Includes a general/global claim (implicit or explicit) about how 
+  qualifications (typically education and work experience) will benefit 
+  the audience. (This often serves as a transition sentence to the next 
+  paragraph.)
+Optional:
+◻	Explains how the applicant heard about the position (only include if it 
+  will add value)
+The Body Paragraph(s)
+◻	Shows specifically how the applicant’s skills and experiences can meet 
+  the needs of his/her audience. (It should complement, rather than 
+  rehash the résumé)
+◻	Discusses relevant accomplishments in terms that are meaningful to the 
+  audience
+Optional:
+◻	Includes a non-obvious comment that makes the reader want to review the 
+  applicant’s résumé
+o	Ex: Points out the THMs* that the applicant wants the reader to draw 
+  from the résumé
+The Closing Paragraph(s):
+◻	Reiterates why the applicant wants the position, in terms of what s/he 
+  can contribute, without being redundant
+◻	Thanks the recipient(s) for taking the time to read the application 
+  materials
+◻	Facilitates the opportunity for an interview
+o	States follow-up intentions and details OR requests that the reader 
+  contact the applicant
+◻	Provides signature block
+*The THMs (or ‘take-home messages’) are the messages that the applicant 
+wants the reader to glean from their application materials about what they 
+have to offer.
+   They include, but are not limited to, the top 5 skills/attributes that 
+   the audience wants.	
+
+WRITING TO EMPLOYERS:
+Cover letters, email, and LinkedIn messages are important tools in 
+presenting yourself professionally to an employer. Make all your 
+correspondence with potential employers professional and respectful, 
+emphasizing the skills and passion you have for a job.
+Cover Letters
+Though an increasing number of employers do not use formal cover letters, 
+many still see cover letters as a powerful way for you to introduce 
+yourself, demonstrate written communication skills, and convince potential 
+employers to read your resume. There are different formats and alterations 
+to use when writing cover letters. The general guidelines and resources 
+available will help you in most situations. Always remember to tailor each 
+cover letter to the needs of your specific audience.
+Before you submit a cover letter, ask a few trusted individuals to review 
+it to help find any errors.
+
+COVEWR LETTER FORMAT GUIDELINES:
+FIRST AND LAST NAME 
+CHAD ORTIZ 
+555.555.5555 | youremail@gmail.com | www.linkedin.com/in/yoururl 
+Use .75 to 1-inch margins and 
+Date 
+Contact’s Name 
+Contact’s Organization 
+Contact’s Street Address 
+City, State, and ZIP Code 
+Dear Contact’s Name: 
+ensure they are balanced from 
+top-to-bottom and side-to-side. 
+Use “[Position Title] Hiring Committee” if 
+you cannot obtain a specific name. Avoid 
+using “To Whom It May Concern”. 
+Use a colon, not a comma. 
+Opening Paragraph: Explain why you are writing and include a powerful 
+audience-relevant ‘grabber’ that shows your vision, company knowledge, 
+and/or how you might fit in this position. It could also include a brief 
+statement about your intended contributions (i.e., how your education and 
+experience could benefit your audience and help them achieve their bottom 
+line.) This claim can also serve as a transition sentence to the next 
+paragraph. You can include personal contact referrals if it will add value. 
+Second Paragraph: Back up any global claims made in the first paragraph 
+with quantitative or qualitative results/impact. Show specifically how your 
+relevant skills, attributes, and experiences can meet your audience’s specific 
+needs. (Do this in a way that complements, rather than rehashes, your 
+resume.) Discuss accomplishments in terms that are meaningful to your 
+audience. 
+Closing Paragraph: Reiterate why you want the position, in a contribution- 
+centered, non-redundant way. Thank the recipient for taking the time to 
+read your application materials. Facilitate the opportunity for an interview in 
+a way that fits well with your audience. Because your contact information is 
+found at the top of your letter, it is redundant to include it here. 
+Best Regards, 
+First and Last Name 
+A signature is only required for ‘hard 
+copy’ submissions. A scanned in 
+signature is an optional nice touch for 
+electronic submissions. 
+555.555.5555 | chad.ortiz@gmail.com | www.linkedin.com/in/chadortiz 
+May 16, 2018 
+Ms. Nancy Stanfield 
+Jefferson Analytics 
+600 S 600 E #100 
+Salt Lake City, UT 84102 
+Dear Ms. Stanfield: 
+Always have 2-3 people proof-read your 
+letter to make sure it is error-free and 
+written effectively. 
+One of my greatest strengths has always been turning complexity into 
+simplicity, without missing any of the important details. Because Jefferson 
+Analytics also shares this trademark, I feel this could be an ideal place for 
+me to further develop this strength while being a vital asset to your data 
+analytics internship team. 
+To ensure I have an ‘on-demand’ and diversified skill-set upon graduation, I 
+have applied knowledge and skills from my analytics and data science 
+courses with two on-campus professional clubs. In  the Business 
+Intelligence Group (BiG), our team used Python, Scikit-learn, PostgreSQL, 
+and SQL to provide recommendations that saved a Fortune 500 tech 
+company $300K upfront and $60K annually. I also helped my ProSales club 
+team to successfully compete at a national competition by researching key 
+product information, managing a sponsor account, and creating 
+communication channels to increase and enhance member participation. 
+I cannot think of a better place than Jefferson Analytics to channel my 
+passion for helping businesses thrive, while learning from highly successful 
+professionals in this field. Thank you for your time in reviewing my 
+application materials. I look forward to hearing from you soon. 
+Best Regards, 
+Chad Ortiz
+
+1. Student Information            
+Kylie Barnes 
+Include your name, phone number, and e-mail address. (Addresses are still 
+standard procedure in many situation; they are becoming optional and 
+discouraged in many situations.) 
+2. Date 
+February 1, 2019 
+Mr. Alfred Barrus 
+Cassia County Prosecuting Attorney 
+Alfred E. Barrus Law Firm 
+1918 Overland Ave. 
+Burley, ID 83318 
+4. Salutation                                         
+Dear Mr. Barrus: 
+3. Employer Information                                            
+Include recipient’s name, title, name of organization, street or 
+P.O. Box address, and city, state, and zip code. 
+Write the letter to the specific person that will be reviewing 
+your materials. Avoid using “To Whom It May Concern” when 
+possible. If you are unsure of the person’s gender, use the 
+person’s entire name (i.e., Dear Alfred Barrus). 
+5. Introductory Paragraph     
+State why you are writing (or the position you are 
+seeking). Then include an attention-grabber, 
+and/or make a global, brief claim about your 
+intended contributions in this position. 
+I am writing in response to the adoption intern position currently available 
+at your firm. As an adopted child with four adopted siblings, my love and 
+appreciation for adoption runs deep. In addition to my passion for adoption, 
+you will also find my education and work experience to be an asset that will 
+benefit your clientele and your office. 
+As a Communication major and Sociology minor at BYU-Idaho, I have completed a 
+wide variety of human behavior courses. I have also served as a volunteer 
+CASA worker for Utah County and a psychosocial rehabilitation specialist in 
+southeastern Idaho. These opportunities have increased my comprehension of 
+the role of a successful case worker, as well as my ability to empathize and 
+connect successfully with parents and children struggling to overcome major 
+life stumbling blocks, while maintaining professional client boundaries.  
+Nothing would bring me greater satisfaction than knowing that my skills and 
+abilities could help place children in secure, loving homes and fill the 
+empty arms of childless parents. I have been inspired by your distinguished 
+and nationally recognized reputation for expertise in adoption. I would love 
+the privilege of learning from the best while offering my time and talents 
+toward your continued success. 
+Thank you for your time in reviewing my materials. I will contact you next 
+week to discuss the possibility of an interview.   
+Sincerely, 
+6. Body Paragraph(s)              
+7. Concluding Paragraph(s)  
+(insert hand-written signature here) 
+Kylie Barnes 
+Enclosure 
+Back up the global claim(s) you just made in 
+the previous paragraph. Specify how your 
+education, work experience, 
+accomplishments, etc. have prepared you to 
+meet the employer’s specific needs. 
+8. Closing                          
+9. Margins                          
+Margins should be between 
+1.0 and .75 inches, and be 
+balanced from top-to
+bottom and side-to-side.  
+Use appropriate closing salutation (i.e., Sincerely, 
+Cordially, Respectfully, Best Regards, Regards, Kind 
+Regards). Avoid using words that create sentimental 
+sounding phrases (i.e., Yours, Truly, Very).  
+Indicate an enclosed document with ‘enclosure’ or an 
+attached document with ‘attachment’.  
+Reiterate why you want the job, in a contribution
+centered way. Thank the recipient for taking the 
+time to read your materials. Facilitate the next step 
+(i.e., state that you will look forward to hearing from 
+them or facilitate an interview opportunity).  
+10. No Errors                          
+Always have 2-3 other people proof-read your letter to 
+make sure that it is error-free. This will also give you the 
+chance to assess if what you have written accurately and 
+effectively expresses the message you are trying to send. 
+
+COVER LETTER SAMPLES:
+TECH COVER LETTER SAMPLE:
+CHAD ORTIZ 
+555.555.5555 | chad.ortiz@gmail.com | www.linkedin.com/in/chadortiz 
+February 21, 2019 
+Ms. Nancy Stanfield 
+Jefferson Analytics 
+600 S 600 E #100 
+Salt Lake City, UT 84102 
+Dear Ms. Stanfield: 
+One of my greatest strengths has always been turning complexity into 
+simplicity, without missing any of the important details. Because Jefferson 
+Analytics also shares this trademark, I feel this could be an ideal place 
+for me to further develop this strength while being a vital asset to your 
+data analytics internship team. 
+To ensure I have an ‘on-demand’ and diversified skill-set upon graduation, 
+I have applied knowledge and skills from my analytics and data science 
+courses with two on-campus professional clubs. In the Business Intelligence 
+Group (BiG), our team used Python, Scikit-learn, PostgreSQL, and SQL to 
+provide recommendations that saved a Fortune 500 tech company $300K upfront 
+and $60K annually. I also helped my ProSales club team to successfully 
+compete at a national competition by finding key product information, 
+managing a sponsor account, and creating communication channels to increase 
+and enhance member participation. 
+I cannot think of a better place than Jefferson Analytics to channel my 
+passion for helping businesses thrive, while learning from highly 
+successful professionals in this field. Thank you for your time in 
+reviewing my application materials. I look forward to hearing from you soon. 
+Best Regards, 
+Chad Ortiz 
+
+EXPERIENCED HIRE COVER LETTER SAMPLE:
+Jonathan J. Hines 
+609-707-9921 • 215-202-1234 • hines_j@nationalre.com • www.linkedin.com/in/jjhines 
+May 16, 2019 
+Mr. Jack Helig, Executive Recruiter 
+Bradley-Kos Careers 
+813 Financial Square 
+Hartford, CT 06101 
+Dear Mr. Helig: 
+Whether you need a forerunner to launch a thriving start-up subdivision, or 
+a technical expert to revitalize and expand an existing site, I can 
+increase company profits, save money for clientele, and create a workforce 
+of happy individuals who constantly surpass expectations. Dedicating my time 
+and talents toward these ends is one of my greatest passions, and has been 
+the motivating and enabling factor of my exceptional and continual success. 
+With this in mind, I can be the vital asset as the WC Managed-Care 
+Marketing/Sales Manager that you are seeking. 
+Throughout my career, one reputation I have consistently earned is that of 
+an ‘industry pioneer.’ As you will see in my résumé, my greatest strengths 
+are my capacities to attain rapid expertise in unknown territories, cause 
+them to flourish in record time, and then train others to achieve similar 
+ends. Most recently, my innovations, with a National Reinsurance (NR) 
+start-up subdivision, had a dominant influence in saving clients $4 million 
+in a little more than 4 years. It also raised program volume by 40%. In 
+addition, I was named ‘Workers’ Compensation Team Leader’ on the most 
+expansive multi-million dollar project review ever conducted by NR. 
+I am confident that my years of marketing and leadership success, and my 
+in-depth knowledge of the worker’s compensation managed-care industry will 
+reap great dividends for all parties that I would work with and serve. May 
+we schedule a time to meet and explore the fit between my background and 
+your needs?  
+Thank you for your time and consideration. 
+Best Regards, 
+Jonathan J. Hines 
+
+HR RECRUITER COVER LETTER SAMPLE:
+Ann E. Body 
+annebody@byui.edu | 208.555.5555 | www.linkedin.com/in/annebody 
+February 9, 2021 
+Jane Jones 
+ABC Insurance Company 
+100 W Town Road Parkway 
+Anytown, State 12345 
+Dear Ms. Jones: 
+Everyone should have an opportunity to find satisfying employment where they 
+land a position that aligns with their strengths and skills. I desire to be 
+a part of a team that recruits talent and places them in positions where 
+they can succeed. That is why I am thrilled to apply for the HR intern 
+position where I can learn from a company who exemplifies these values and 
+contribute the skills I have worked hard to develop to your organization.  
+As a Communication major and Psychology minor, I have completed a wide 
+variety of classes that have taught me strong interpersonal and 
+communication skills. I also have demonstrated strong organizational skills 
+in my previous work experiences. While working as an office assistant I 
+learned how to quickly manage and organize the high volume of incoming 
+requests using applications like Excel in Microsoft Office. As an employee 
+at Disney World, I was recognized for my outstanding attendance record. In 
+addition, while attending school, I trained and developed a team of 14 
+volunteers on all procedures for running and executing a successful 
+freshman orientation.  
+am confident that my experience in training, strong organizational skills, 
+and ability to communicate will contribute to your team, and my excitement 
+for the position will allow me to develop and grow in a field I am 
+passionate about. Thank you for taking the time to read through my 
+application materials and I welcome an opportunity to discuss the 
+possibility of an interview.  
+Ann E. Body 
+`};
